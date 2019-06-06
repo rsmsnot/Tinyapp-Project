23e24c486dd4783f937c949be809adae882e6936
rsmsnot <rsmsnot@outlook.com>
2019-06-06 23:28:27 +0000

    registration errors

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
-let userName = '';
+let user_id = '';
 app.use(cookieParser())
 app.set("view engine", "ejs")
 
@@ -13,6 +13,19 @@ var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+    "userRandomID": {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk"
+    }
+}
 
 function generateRandomString() {
     let characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
@@ -31,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/urls/new", (req, res) => {
     let templateVars = {
-        username: req.cookies["username"],
+        user_id: req.cookies["user_id"],
     };
     res.render("urls_new", templateVars);
 });
@@ -52,7 +65,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls", (req, res) => {
     let templateVars = {
-        username: req.cookies["username"],
+        user_id: req.cookies["user_id"],
         urls: urlDatabase
     };
     res.render("urls_index", templateVars);
@@ -60,7 +73,7 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = {
-        username: req.cookies["username"],
+        user_id: req.cookies["user_id"],
         shortURL: req.params.shortURL,
         longURL: urlDatabase
     };
@@ -100,18 +113,52 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+// LOGIN / LOGOUT
+
 app.post("/login", (req, res) => {
-    // console.log("username: ", req.body.username)
-    userName = req.body.username;
-    res.cookie('username', userName);
+    // console.log("user_id: ", req.body.user_id)
+    user_id = req.body.user_id;
+    res.cookie('user_id', user_id);
     res.redirect('/urls')
 });
 
 app.post("/logout", (req, res) => {
-    res.clearCookie('username', userName);
+    res.clearCookie('user_id', user_id);
     res.redirect('/urls')
 })
 
+//REGISTRATION
+
+let userAuthentication = (email, password) => {
+    for (id in users) {
+        let currentUser = users[id].email;
+        console.log("hello", email, currentUser)
+        if (!email || !password) {
+            return false;
+        } else if (currentUser === email) {
+            console.log("match found")
+            return false;
+        }
+        console.log("no match found");
+        return true;
+    }
+}
+
 app.get("/register", (req, res) => {
     res.render("urls_registration")
+});
+
+app.post("/register", (req, res) => {
+    let id = generateRandomString();
+    let email = req.body.email;
+    let password = req.body.password;
+    let validUser = userAuthentication(email, password)
+    if (!validUser) {
+        res.send("Please enter a valid email and password");
+        return;
+    }
+    users[id] = { id: id, password: password, email: email }
+    res.cookie('user_id', id);
+    res.redirect('/urls');
 });
